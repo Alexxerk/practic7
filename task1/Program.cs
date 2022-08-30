2270e6f9c9b51d2a0f83d8021957966e1aeec817
Alex <newalexis777@gmail.com>
2022-08-30 21:30:38 +0400

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,56 @@
+﻿//Найходим введеное число и выдаем его позицию.
+
+Console.WriteLine("Введите размер двумерного массива: ");
+Console.WriteLine("Введите x: ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите y: ");
+int y = int.Parse(Console.ReadLine());
+int[,] matrix = new int[x, y];
+Console.WriteLine("Введите искомое число: ");
+int z = int.Parse(Console.ReadLine());
+
+void FillAray(int[,] one)
+{
+    for (int i = 0; i < one.GetLength(0); i++)
+    {
+        for (int j = 0; j < one.GetLength(1); j++)
+        {
+            one[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintAray(int[,] two)
+{
+    for (int i = 0; i < two.GetLength(0); i++)
+    {
+        for (int j = 0; j < two.GetLength(1); j++)
+        {
+            Console.Write(two[i, j]);
+            Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindNumber(int[,] three, int find)
+{
+    for (int i = 0; i < three.GetLength(0); i++)
+    {
+        for (int j = 0; j < three.GetLength(1); j++)
+        {
+            if(three[i, j] == find)
+            {
+                Console.WriteLine("Найденный элемент " + find + " находиться в [" + i + ", " + j + "]");
+                return;
+            }
+        }
+    }
+    Console.WriteLine("В данном массиве нет даного числа. ");
+}
+
+FillAray(matrix);
+PrintAray(matrix);
+Console.WriteLine();
+FindNumber(matrix, z);
+
